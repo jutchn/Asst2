90b3472955cce990db06feeb90f8c6f615ac293a
jutchn <darkreaper411@gmail.com>
2020-11-29 19:21:38 -0500

    Update test3.c

--- test3.c
+++ test3.c
@@ -10,10 +10,6 @@
 #include <unistd.h>
 #include <math.h>
 
-#define HASHSIZE 10
-
-pthread_mutex_t lock;
-
 struct ListNode {
     char* str;
     float freq;
@@ -25,260 +21,6 @@ struct FileNode {
     struct ListNode** tokens;
     struct FileNode* next;
 };
-
-struct token_struct{
-   int* arg1;
-   pthread_mutex_t* arg2;
-}token;
-
-typedef struct folder_struct
-{
-   char* buf;
-   pthread_mutex_t* mutex;
-}folder;
-
-/*
-int getHashKey(char* str) {
-    int val = 0;
-    for (int i = 0; i < strlen(str); i++) {
-        val += (int)(str[i]);
-    }
-    return val % HASHSIZE;
-}
-
-struct ListNode* searchList(struct ListNode* head, char* target) {
-    struct ListNode* temp = head;
-    while (temp != NULL) {
-        if (strcmp(temp->str, target) == 0) {
-            return temp;
-        }
-        temp = temp->next;
-    }
-    return NULL;
-}
-
-struct FileNode* searchFile(struct FileNode* head, char* target) {
-    struct FileNode* temp = head;
-    while (temp != NULL) {
-        if (strcmp(temp->str, target) == 0) {
-            return temp;
-        }
-        temp = temp->next;
-    }
-    return NULL;
-}
-
-struct ListNode* searchHash(struct ListNode** hash, char* target) {
-    int key = getHashKey(target);
-    return searchList(hash[key], target);
-}
-
-struct ListNode* addFile(struct FileNode* head, char* string, float count) {
-    if (head == NULL) {
-        struct FileNode* newNode = (struct FileNode*)malloc(sizeof(struct FileNode));
-        newNode->str = (char*)malloc(sizeof(char) * (strlen(string) + 1));
-        strcpy(newNode->str, string);
-        newNode->tokens = (struct ListNode**)malloc(sizeof(struct ListNode*) * HASHSIZE);
-        for (int i = 0; i < HASHSIZE; i++) {
-            newNode->tokens[i] = NULL;
-        }
-        newNode->count = count;
-        newNode->next = NULL;
-        return newNode;
-    }
-    struct FileNode* temp = searchFile(head, string);
-    if (temp == NULL) {
-        struct FileNode* newNode = (struct ListNode*)malloc(sizeof(struct FileNode));
-        newNode->str = (char*)malloc(sizeof(char) * (strlen(string) + 1));
-        strcpy(newNode->str, string);
-        newNode->tokens = (char**)malloc(sizeof(char*) * HASHSIZE);
-        for (int i = 0; i < HASHSIZE; i++) {
-            newNode->tokens[i] = NULL;
-        }
-        newNode->count = count;
-        newNode->next = head;
-        return newNode;
-    }
-    else {
-        return head;
-    }
-}
-
-struct ListNode* addList(struct ListNode* head, char* string, float count) {
-    if (head == NULL) {
-        struct ListNode* newNode = (struct ListNode*)malloc(sizeof(struct ListNode));
-        newNode->str = (char*)malloc(sizeof(char) * (strlen(string) + 1));
-        strcpy(newNode->str, string);
-        newNode->freq = count;
-        newNode->next = NULL;
-        return newNode;
-    }
-    struct ListNode* temp = searchList(head, string);
-    if (temp == NULL) {
-        struct ListNode* newNode = (struct ListNode*)malloc(sizeof(struct ListNode));
-        newNode->str = (char*)malloc(sizeof(char) * (strlen(string) + 1));
-        strcpy(newNode->str, string);
-        newNode->freq = count;
-        newNode->next = head;
-        return newNode;
-    }
-    else {
-        temp->freq += count;
-        return head;
-    }
-}
-
-struct ListNode** addHash(struct ListNode** hash, char* target, float count) {
-    int key = getHashKey(target);
-    hash[key] = addList(hash[key], target, count);
-    return hash;
-}
-
-void printList(struct ListNode* head) {
-    if (head == NULL) {
-        printf("NULL\n");
-        return;
-    }
-    struct ListNode* temp = head;
-    while (temp != NULL) {
-        printf("\"%s\" %d\n", temp->str, temp->freq);
-        temp = temp->next;
-    }
-}
-
-void printHash(struct ListNode** hash) {
-    for (int i = 0; i < HASHSIZE; i++) {
-        printf("[%d]\n", i + 1);
-        printList(hash[i]);
-    }
-    return;
-}
-
-void printFiles(struct FileNode* head) {
-    if (head == NULL) {
-        printf("NULL\n");
-        return;
-    }
-    struct FileNode* temp = head;
-    while (temp != NULL) {
-        printf("\"%s\" %d ----------------------------\n", temp->str, temp->count);
-        printHash(temp->tokens);
-        temp = temp->next;
-    }
-}
-
-struct FilePair {
-    char* file1;
-    char* file2;
-    float distance;
-};
-
-struct FilePair* jsDistance(struct FileNode* first, struct FileNode* second) {
-    if (first == NULL || second == NULL) {
-        return NULL;
-    }
-    struct ListNode** firstList = first->tokens;
-    struct ListNode** secondList = second->tokens;
-    
-    float KLD1 = 0;
-    float KLD2 = 0;
-    char** checkedStrings = (char**) malloc(sizeof(char*) * (first->count + second->count));
-    int checkedStringsCounter = 0;
-    for (int i = 0; i < HASHSIZE; i++) {
-        struct ListNode* first1 = firstList[i];
-        while (first1 != NULL) {
-            struct ListNode* second1 = searchHash(secondList, first1->str);
-            checkedStrings[checkedStringsCounter] = (char*)malloc(sizeof(char) * strlen(first1->str));
-            strcpy(checkedStrings[checkedStringsCounter++], first1->str);
-
-            if (second1 == NULL) {
-                float mean = (first1->freq) / 2;
-                KLD1 += (first1->freq) * log10(first1->freq / mean);
-            }
-            else {
-                float mean = (first1->freq + second1->freq) / 2;
-                KLD1 += (first1->freq) * log10(first1->freq / mean);
-                KLD2 += (second1->freq) * log10(second1->freq / mean);
-            }
-            //printf("%f\n%f\n", KLD1, KLD2);
-            first1 = first1->next;
-        }
-    }
-    for (int i = 0; i < HASHSIZE; i++) {
-        struct ListNode* first2 = secondList[i];
-        while (first2 != NULL) {
-            int checked = 0;
-            for (int i = 0; i < checkedStringsCounter; i++) {
-                if (strcmp(checkedStrings[i], first2->str) == 0) {
-                    checked = 1;
-                    break;
-                }
-            }
-            if (!checked) {
-                struct ListNode* second2 = searchHash(firstList, first2->str);
-                if (second2 == NULL) {
-                    float mean = (first2->freq) / 2;
-                    KLD1 += (first2->freq) * log10(first2->freq / mean);
-                }
-                else {
-                    float mean = (first2->freq + second2->freq) / 2;
-                    KLD1 += (first2->freq) * log10(first2->freq / mean);
-                    KLD2 += (second2->freq) * log10(second2->freq / mean);
-                }
-            }
-
-            first2 = first2->next;
-        }
-
-    }
-
-    struct FilePair* pair = (struct FilePair*)malloc(sizeof(struct FilePair));
-    pair->file1 = first->str;
-    pair->file2 = second->str;
-    pair->distance = (KLD1 + KLD2) / 2;
-    
-    return(pair);
-}
-void printPairs(struct FileNode* head) {
-    struct FileNode* first = head;
-    while (first->next != NULL) {
-        struct FileNode* second = first->next;
-        while (second != NULL) {
-            struct FilePair* pair = jsDistance(first, second);
-            if (pair->distance <= 0.1) {
-                printf("\033[0;31m");
-                printf("%0.4f ", pair->distance);
-                printf("\033[0m");
-            }
-            else if (pair->distance <= 0.15) {
-                printf("\033[0;33m");
-                printf("%0.4f ", pair->distance);
-                printf("\033[0m");
-            }
-            else if (pair->distance <= 0.2) {
-                printf("\033[0;32m");
-                printf("%0.4f ", pair->distance);
-                printf("\033[0m");
-            }
-            else if (pair->distance <= 0.25) {
-                printf("\033[0;36m");
-                printf("%0.4f ", pair->distance);
-                printf("\033[0m");
-            }
-            else if (pair->distance <= 0.3) {
-                printf("\033[0;34m");
-                printf("%0.4f ", pair->distance);
-                printf("\033[0m");
-            } else{
-                printf("%0.4f ", pair->distance);
-            }
-            printf("\"%s\" and \"%s\" \n", pair->file1, pair->file2);
-            second = second->next;
-        }
-        first = first->next;
-    }
-}
-*/
 
 typedef struct pthreads {
     pthread_t* thread_id;
@@ -293,32 +35,61 @@ typedef struct tInput {
 }tInput;
 
 
-
-
-
-pthreads* addThread(pthreads *head, pthread_t* new){
+void addThread(pthreads **head, pthread_t* new){    
+    pthreads* ref = *head;
     pthreads* newThread = (pthreads*)malloc(sizeof(pthreads));
     newThread->thread_id = new;
     newThread->next = NULL;
     int counter = 0;
-    if(head == NULL){
-        return newThread;
-    }
-    pthreads* ref = head;
+    if(*head == NULL){
+        *head = newThread;
+        printf("    1st threads added\n");
+        return;
+    } 
     while(ref->next != NULL){
         counter+=1;
         ref = ref->next;
     }
     ref->next = newThread;
-    printf("   added thread %d\n",counter);
-    return head;
+    printf("    %dth threads added\n",counter+2);
+    return;
 }
 
-void printThread(pthreads *head){
+ #define handle_error_en(en, msg) \
+               do { errno = en; perror(msg); exit(EXIT_FAILURE); } while (0)
+
+void* joinThread(pthreads** phead){
+    //printf("piss\n");
+    // if(phead == NULL){
+    //     printf("fuuuuuuuu");
+    // }
+    int count = 0;
+    //printf("|||%d|||\n",count);
+    pthreads* ref = *phead;
+    pthreads* prev;
+    while(ref != NULL){
+        prev = ref;
+        count+=1;
+        pthread_t* thread_id = ref->thread_id;
+        int a = pthread_join(*thread_id, NULL);
+        if(a!=0){
+            handle_error_en(a, "pthread_join");
+        }
+        ref = ref->next;
+        free(prev);
+    }
+
+    //printf("|||%d|||\n",count);
+}
+
+
+void printThread(pthreads **head){
     printf("counting threads\n");
     int count = 0;
-    while(head->next != NULL){
+    pthreads* ref = *head;
+    while(ref != NULL){
         count +=1;
+        ref = ref->next;
     }
     printf("count is %d\n",count);
 }
@@ -329,21 +100,21 @@ int validDir(char* dirname){
         printf("Cannot open %s\n", dirname);
         return -1;
     }
-     else return 1;
+    
+     else {
+     closedir(dir);
+     return 1;
+     }
 }
 
 void* handleDir(void *vinput){
-    printf("peeee");
-    //printf("thread id is %d\n", pthread_self());
+    printf("                   thread id is %d\n", pthread_self());
     tInput *input = vinput;
     char* dirname = input->name;
-    
-    printf("%s\n",input->name);
-
+    printf("File path: %s\n",input->name);
     if(validDir(dirname) == -1){
         return NULL;
     }
-    
     DIR* dir = opendir(dirname);
     struct dirent *dp;
     struct stat path_stat;
@@ -352,87 +123,110 @@ void* handleDir(void *vinput){
         if(strcmp(dp->d_name, ".") == 0 || strcmp(dp->d_name, "..") == 0){
             continue;
         }
-        int size = strlen(dirname) + strlen(dp->d_name) + 1;
-        char *buf = malloc(size*sizeof(char));
-        strcpy(buf,dirname);
-        strcat(buf,"/");
-        strcat(buf,dp->d_name);
-        stat(buf, &path_stat);
-        
         tInput* new = (tInput*)malloc(sizeof(tInput));
-        new->name = buf;
+        int size = strlen(dirname) + strlen(dp->d_name) + 2;
+        new->name = (char*)malloc(size*sizeof(char));
+        for(int i = 0; i<size-1; i++){
+            if(i<strlen(dirname)){
+                new->name[i] = dirname[i];
+            } else if(i == strlen(dirname)){
+                new->name[i] = '/';
+            } else {
+                new->name[i] = (dp->d_name)[i-strlen(dirname)-1];
+            }
+        } new->name[size-1] = '\0';
+
+        stat(new->name, &path_stat);
+        printf("((%s))--------------------------------------------------\n",new->name);
+        
         new->phead = input->phead;
         new->front = input->front;
         new->lock = input->lock;
         
+        pthread_t* thread_id;
         // is a directory
         if (S_ISDIR(path_stat.st_mode)){
-            pthread_t thread_id;
             pthread_mutex_lock(new->lock);
-            new->phead = addThread(new->phead, &thread_id);
+            addThread(&(new->phead), thread_id);
             pthread_mutex_unlock(new->lock);
-            pthread_create(&thread_id, NULL, handleDir, (void*)new);
-            //handleDir((void*)new);
+            pthread_create(thread_id, NULL, handleDir, (void*)new);
         // is a regular file
         } else if (S_ISREG(path_stat.st_mode)) {
-            pthread_t thread_id;
+            
             pthread_mutex_lock(new->lock);
-            new->phead = addThread(new->phead, &thread_id);
+            addThread(&(new->phead), thread_id);
             pthread_mutex_unlock(new->lock);
 
             printf("goin into file %s\n", new->name);
             //pthread_create(&thread_id, NULL, handleFile, new);
         }
-    }    
-    closedir(dir);
+    }
     free(input->name);
     free(input);
+    closedir(dir);
     pthread_exit(NULL);
 }
-
-void joinThread(pthreads* phead){
-    while(phead->next != NULL){
-        pthread_join(*(phead->thread_id), NULL);
-    }
-}
-
 
 
 
 
 int main(int argc, char **argv){
-    
+
     // Read in directory and error if invalid number of arguments
     if(argc != 2){
-        printf("Invalid number of arguments");
+//        printf("Invalid number of arguments");
         return -1;
     }
     char *dirname = argv[1];
     if(validDir(dirname) == -1){
-        printf("Invalid folder\n");
+//        printf("Invalid folder\n");
         exit(0);
     }
-    
-    tInput* input = (tInput*)malloc(sizeof(tInput)); 
-    pthreads* phead = NULL;
-    
-    pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
+    tInput* input = (tInput*)malloc(sizeof(tInput));
+    input->name = (char*)malloc(sizeof(char)*(strlen(dirname)));
+    pthreads* phead = NULL;
+
+    pthread_mutex_t* mutex = malloc(sizeof(pthread_mutex_t));
+    pthread_mutex_t mutexe = PTHREAD_MUTEX_INITIALIZER;
+    *mutex = mutexe;
     struct FileNode* front = NULL;
 
     input->front = front;
-    input->lock = &mutex;
-    input->name = dirname;
-    input->phead = phead; 
+    input->lock = mutex;
+    strcpy(input->name, dirname);
+    input->phead = phead;
+    //printf("oooooooo\n");
     //printf("%s\n",input->name);
-    pthread_t thread_id;
-    input->phead = addThread(input->phead, &thread_id);
-    pthread_create(&thread_id, NULL, handleDir, (void*)input);
+    pthread_t* thread_id = malloc(sizeof(pthread_t));
+    pthread_mutex_lock(input->lock);
+
+    addThread(&(input->phead), thread_id);
+
+    pthread_mutex_unlock(input->lock);
+    //printThread(&(input->phead));
+
+    pthread_create(thread_id, NULL, handleDir, (void*)input);
     //printThread(phead);
     
+    //joinThread(&(input->phead));
+
     
+    //printf("|||%d|||\n",count);
+    pthreads* ref = input->phead;
+    pthreads* prev;
+    while(ref != NULL){
+        prev = ref;
+        pthread_t* thread_id = ref->thread_id;
+        int a = pthread_join(*thread_id, NULL);
+        if(a!=0){
+            handle_error_en(a, "pthread_join");
+        }
+        ref = ref->next;
+        free(prev);
+    }
     
-    joinThread(phead);
+    pthread_mutex_destroy(input->lock);
+    
     //free(input);
-    printf("the n word");
 }
